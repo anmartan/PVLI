85b40ad62584ed26556c8baf006d6ffb0cc939de
Drathijin <rsulbara@ucm.es>
2019-10-18 11:43:51 +0200

    añadiendo animación al jugador

--- scripts/scene1.js
+++ scripts/scene1.js
@@ -3,11 +3,28 @@ const scene = {
 
     preload: function()
    {
-       this.load.image("caballero", "../assets/player/knight_m_idle_anim_f0.png")
+       this.load.image("caballero_idle0", "../assets/player/knight_m_idle_anim_f0.png")
+       this.load.image("caballero_idle1", "../assets/player/knight_m_idle_anim_f1.png")
+       this.load.image("caballero_idle2", "../assets/player/knight_m_idle_anim_f2.png")
+       this.load.image("caballero_idle3", "../assets/player/knight_m_idle_anim_f3.png")
    },
     create: function()
-   {
-       this.player = this.add.image(50,50,"caballero");
+    {
+        
+        this.anims.create({
+         key: 'idle',
+         frames:
+         [
+             {key: "caballero_idle0"},
+             {key: "caballero_idle1"},
+             {key: "caballero_idle2"},
+             {key: "caballero_idle3"},
+         ],
+         frameRate: 10,
+         repeat: -1,
+         });
+       this.player = this.add.sprite(50,50,"caballero").play("idle");
+       this.player.setScale(1.5,1.5); //no se si hace falta pero yo lo veo mejor creo
        this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
        this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
        this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
@@ -20,10 +37,14 @@ const scene = {
        if(this.key_D.isDown)
        {
            this.player.x+= this.player.velocidad;
-       }
-       if(this.key_A.isDown)
-       {
-           this.player.x-= this.player.velocidad;
+           this.player.setFlipX(false);
+
+        }
+        if(this.key_A.isDown)
+        {
+            this.player.x-= this.player.velocidad;
+            this.player.setFlipX(true);
+           
        }
        if(this.key_W.isDown)
        {
