92e65d74ed025f69205c59e8876ad49c55e764f1
Drathijin <rsulbara@ucm.es>
2019-10-20 20:18:02 +0200

    falta automatizar mejor el proceso de creación de mazmorras que está programado a capón por ahora

--- scripts/dungeon.js
+++ scripts/dungeon.js
@@ -1,4 +1,4 @@
-export class dungeon
+export  class dungeon
 {
     constructor(rooms)
     {
@@ -13,6 +13,131 @@ export class room
         this.height = height;
         this.traps = traps;
         this.enemies = enemies;
+    }
+    show(scene)
+    {
+        let tileMap = scene.add.tilemap("tiles");
+        this.DungeonTiles = tileMap.addTilesetImage("DungeonTiles");
+        this.Background = tileMap.createDynamicLayer("Background", [this.DungeonTiles],0,0);
+        this.Ground = tileMap.createDynamicLayer("Ground", [this.DungeonTiles],0,0);
+        this.Walls = tileMap.createDynamicLayer("Walls", [this.DungeonTiles],0,0);
+    }
+    make5x5()
+    {
+        this.Walls.putTileAt(1,2,2);
+        this.Walls.putTileAt(3,8,2);
+        this.Walls.putTileAt(15,8,8);
+        this.Walls.putTileAt(13,2,8);
+        for(let i=0;i<5;i++)
+        {
+            this.Walls.putTileAt(2, i+3,   2);
+            this.Walls.putTileAt(7,   2, i+3);
+            for(let j = 0; j<5;j++)
+            {
+                this.Walls.removeTileAt(i+3, j+3);
+                this.Ground.putTileAt(8, i+3, j+3);
+            }
+            this.Walls.putTileAt(14, i+3, 8);
+            this.Walls.putTileAt(9,   8, i+3);
+        }
+        this.Walls.removeTileAt( 2, 5 );
+        this.Walls.removeTileAt( 8, 5);
+        this.Ground.putTileAt(8,2,5);
+        this.Ground.putTileAt(8,7,5);
+        this.removeRounds5();
+    }
+    make7x7()
+    {
+        this.Walls.putTileAt(1,1,1);
+        this.Walls.putTileAt(3,9,1);
+        this.Walls.putTileAt(15,9,9);
+        this.Walls.putTileAt(13,1,9);
+        for(let i=0;i<7;i++)
+        {
+            this.Walls.putTileAt(2, i+2,   1);
+            this.Walls.putTileAt(7,   1, i+2);
+            for(let j = 0; j<7;j++)
+            {
+                this.Walls.removeTileAt(i+2, j+2);
+                this.Ground.putTileAt(8, i+2, j+2);
+            }
+            this.Walls.putTileAt(14, i+2, 9);
+            this.Walls.putTileAt(9,   9, i+2);
+        }
+        this.Walls.removeTileAt( 1, 5 );
+        this.Walls.removeTileAt( 9, 5);
+        this.Ground.putTileAt(8,1,5);
+        this.Ground.putTileAt(8,9,5);
+        this.removeRounds7();
+    }
+    make9x9()
+    {
+        this.Walls.putTileAt(1,0,0);
+        this.Walls.putTileAt(3,10,0);
+        this.Walls.putTileAt(15,10,10);
+        this.Walls.putTileAt(13,0,10);
+        for(let i=0;i<9;i++)
+        {
+            this.Walls.putTileAt(2, i+1,   0);
+            this.Walls.putTileAt(7,   0, i+1);
+            for(let j = 0; j<9;j++)
+            {
+                this.Walls.removeTileAt(i+1, j+1);
+                this.Ground.putTileAt(8, i+1, j+1);
+            }
+            this.Walls.putTileAt(14, i+1, 10);
+            this.Walls.putTileAt(9,   10, i+1);
+        }
+        this.Walls.removeTileAt( 1, 6 );
+        this.Walls.removeTileAt( 9, 6);
+        this.Ground.putTileAt(8,1,6);
+        this.Ground.putTileAt(8,10,6);
+    }
+    removeRounds7()
+    {
+        let x = 0, y=0;
+        for(y = 0; y<11; y++)
+        {
+            x = 0;
+            this.removeTile(x,y);
+            x = 10;
+            this.removeTile(x,y);            
+        }
+        y = 0;
+        for(x = 0; x<11;x++)
+        {
+            y = 0;
+            this.removeTile(x,y);            
+            y=10;
+            this.removeTile(x,y);            
+        }
+    }
+    removeRounds5()
+    {
+        this.removeRounds7();
+        let x = 1, y=1;
+        for(y = 1; y<10; y++)
+        {
+            x = 1;
+            this.removeTile(x,y);
+            x = 9;
+            this.removeTile(x,y);            
+        }
+        y = 1;
+        for(x = 1; x<10; x++)
+        {
+            y = 1;
+            this.removeTile(x,y);            
+            y = 9;
+            this.removeTile(x,y);            
+        }
+
+    }
+    removeTile(x,y)
+    {
+        this.Walls.removeTileAt(x,y);
+        this.Ground.removeTileAt(x,y);
+        this.Background.putTileAt(6,x,y);
     }
 }
 export class trap extends Phaser.GameObjects.Sprite
--- scripts/scene2.js
+++ scripts/scene2.js
@@ -7,47 +7,35 @@ const  scene =
         this.load.image("DungeonTiles","../assets/ground/DungeonStarter.png");
         this.load.tilemapTiledJSON("tiles","../assets/ground/tiles.json");
         
-        let rooms = [ new room(5,5,0,0),new room(7,7,0,0), new room(9,9,0,0) ];
-        this.dungeon = new dungeon(rooms);
+        this.rooms = [ new room(5,5,0,0),new room(7,7,0,0), new room(9,9,0,0) ];
+        this.dungeon = new dungeon(this.rooms);
         
     },
     create : function()
     {
         
-        
-        let tiles5x5 = this.add.tilemap("tiles");
-        console.log(tiles5x5);
-        let DungeonTiles = tiles5x5.addTilesetImage("DungeonTiles");
-        let Background = tiles5x5.createDynamicLayer("Background", [DungeonTiles],0,0);
-        let Ground = tiles5x5.createDynamicLayer("Ground", [DungeonTiles],0,0);
-        let Walls = tiles5x5.createDynamicLayer("Walls", [DungeonTiles],0,0);
-        
-        
-        //si quitas este comentario se hace de 7x7
-        /*Walls.putTileAt(1,1,1);
-        Walls.putTileAt(3,9,1);
-        Walls.putTileAt(15,9,9);
-        Walls.putTileAt(13,1,9);
-        for(let i=0;i<7;i++)
-        {
-            Walls.putTileAt(2, i+2,   1);
-            Walls.putTileAt(7,   1, i+2);
-            for(let j = 0; j<7;j++)
-            {
-                Walls.removeTileAt(i+2, j+2);
-                Ground.putTileAt(8, i+2, j+2);
-            }
-            Walls.putTileAt(14, i+2, 9);
-            Walls.putTileAt(9,   9, i+2);
-        }
-        Walls.removeTileAt( 1, 5 );
-        Walls.removeTileAt( 9, 5);
-        Ground.putTileAt(8,1,5);
-        Ground.putTileAt(8,9,5);*/
+        this.rooms[0].show(this);
+        //this.rooms[0].make7x7();
+        //this.rooms[0].make9x9();
 
+        let text = this.add.text(25, 100, 'Small');
+        text.setFont('Arial Black');
+        text.setFontSize(12);
+        let text1 = this.add.text(70, 100, 'Medium');
+        text1.setFont('Arial Black');
+        text1.setFontSize(12);
+        let text2 = this.add.text(125, 100, 'Large');
+        text2.setFont('Arial Black');
+        text2.setFontSize(12);
 
+        
+        text.setInteractive();
+        text1.setInteractive();
+        text2.setInteractive();
+        text.on("pointerdown",()=>this.rooms[0].make5x5());
+        text1.on("pointerdown",()=>this.rooms[0].make7x7());
+        text2.on("pointerdown",()=>this.rooms[0].make9x9());
     },
-
     update : function(delta)
     {
         
