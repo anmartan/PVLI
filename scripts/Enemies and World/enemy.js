d0c2428900b26a396bbfc02b11a51f97608f0037
anmartan <anmartan@ucm.es>
2019-12-02 12:36:11 +0100

    Los enemigos ya hacen daño al héroe

--- scripts/Enemies and World/enemy.js
+++ scripts/Enemies and World/enemy.js
@@ -129,7 +129,7 @@ export class zombie extends enemy
 
    attackEffect(player)
    {
-       console.log ("Salud del héroe: " + player.health());
+       console.log ("Salud del héroe: " + player.health);
        player.damage(1);
        console.log("Estoy atacando al héroe: " + player.health);
    }
@@ -350,7 +350,7 @@ export class enemyManager
             scene.physics.add.overlap(weaponGroup, this.enemies, (weapon, enemy) => {hero.attack(enemy, weapon.Damage)});         //
             scene.physics.add.overlap(this.zone, hero , (zone) => zone.parent.spotPlayer(hero));                  //
             scene.physics.add.collider(this.enemies, walls);                                                              // TODO: En un mundo ideal se le pasará un objeto config a la constructora de zombie con todo esto
-            scene.physics.add.collider(this.enemies, hero);                                                               //       que se encargará de crear todas las colisiones correspondientes y quedará mucho más limpio
+            scene.physics.add.collider(this.enemies, hero, (enemy)=>{enemy.attack(hero, 1)});                                                               //       que se encargará de crear todas las colisiones correspondientes y quedará mucho más limpio
     
             this.summoned = true;
             for(let i = 0; i<this.enemiesInfo.length;i++)
