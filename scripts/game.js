e729eb6ae446ba0c493cbfb068689c0ec8a214c0
Drathijin <rsulbara@ucm.es>
2019-12-14 16:39:22 +0100

    arreglando las movidas de git ;-;

--- scripts/game.js
+++ scripts/game.js
@@ -3,13 +3,14 @@ import DungeonRun from './Scenes/dungeonRun.js'
 import DungeonRunAH from  './Scenes/dungeonRunAH.js'
 import SelectGameMode from './Scenes/selectMode.js'
 import ItemShop from "./Scenes/itemShop.js"
-
-let scenes = [SelectGameMode,ItemShop, DungeonEditor, DungeonRun,DungeonRunAH]
+let tileSize = 32;
+let scenes = [SelectGameMode, ItemShop, DungeonEditor, DungeonRun,DungeonRunAH]
 let config = {
     pixelArt: true,
     type: Phaser.AUTO,
-    width: 176,//960,
-    height: 176,
+    width: 11*tileSize,//960,
+    height: 11*tileSize,
+    backgroundColor:"#181425",
     scale: {   //Esto sirve para centrar el canvas
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_HORIZONTALY
@@ -22,11 +23,24 @@ let config = {
         }
     },
     scene: scenes,
-
 }
 
-let  game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 game.dungeon;
 game.tiles;
+game.tileSize = tileSize;
+game.fromTileToPixel = 
+function fromTileToPixel(tileX, tileY, scale=1, offsetX=0,offsetY=0)
+{
+
+}
+game.fromPixelToTile = 
+function fromPixelToTile(pos, scale=1, offsetX=0,offsetY=0)
+{
+    pos.x = pos.x/tileSize;
+    pos.x-1.5;//(Si se utiliza el centro como pivote)
+    pos.y = pos.y/tileSize;
+    pos.y-1.5;//(Si se utiliza el centro como pivote)
+}
 
 
