f2ba7c57f797356b5b8ba928cfdc53694d7f98be
Drathijin <rsulbara@ucm.es>
2019-12-15 19:47:21 +0100

    Cambiando el atlas, que Dios nos pille confesados

--- scripts/Player and Items/itemAtlas.js
+++ scripts/Player and Items/itemAtlas.js
@@ -1,357 +1,263 @@
 export const itemAtlas = 
 {
-    Sword_0 :
+    /*
+    tipos:
+        weapon:
+            -Damage
+            -Cooldown
+        proyectile:
+            -Damage
+        potion:
+            -Health
+        radar:
+            -Area
+        armor:
+            -ExtraMaxHealth
+        shield:
+            -MaxHits
+        boots:
+            -Speed
+    price:
+    level:
+    images:
+        sprite: "SpriteName",
+        icon:   "IconName"
+    */
+    Armor1:
     {
-        Consumible: false,
-        Level: 0,
-        Price: 0,
-        Effect:
+        Type: "Armor",
+        ExtraMaxHealth:0,
+        Price:0,
+        Level:"",
+        Images:
         {
-            Target: "other",
-            Data:
-            {
-                Attribute: "damage",
-                Quantity:1,
-                Cooldown:0
-            }
-        },
-        Sprite:
-        {
-            ID : "Sword_0",
-            Scale: 0.5,
-        }    
-    },
-    Sword_1 :
-    {
-        Consumible: false,
-        Level: 1,
-        Price: 15,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-                Attribute: "damage",
-                Quantity:2,              //+2 pto ataque
-                Cooldown:0
-            }
-        },
-        Sprite:
-        {
-            ID : "Sword_1",
-            Scale: 0.5,
-        }    
-    },
-    
-    Sword_2 :
-    {
-        Consumible: false,
-        Level: 2,
-        Price: 30,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-                Attribute: "damage",
-                Quantity:3,             //+2 pto ataque
-                Cooldown:0
-            }
-        },
-        Sprite:
-        {
-            ID : "Sword_2",
-            Scale: 0.5,
-        }  
-    
-    },
-    //Cómo indicamos el daño de área aquí????
-    Sword_3 :
-    {
-        Consumible: false,
-        Level: 3,
-        Price: 60,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-                Attribute: "damage",
-                Quantity:3,
-                Cooldown:0
-                //daño de área
-            }
-        },
-        Sprite:
-        {
-            ID : "Sword_3",
-            Scale: 0.5,
-        }  
-    
-    },
-    
-    Armor_1 :
-    {
-        Consumible: false,
-        Level: 1,
-        Price: 20,
-        Effect:
-        {
-            Target: "self",
-            Data:
-            {
-                Attribute: "health",
-                Quantity: 2,        // +2 pto vida máxima
-                Cooldown: 0
-            }
-        }
-    
-    },
-    Armor_2 :
-    {
-        Consumible: false,
-        Level: 2,
-        Price: 40,
-        Effect:
-        {
-            Target: "self",
-            Data:
-            {
-                Attribute: "health",
-                Quantity: 4,        // +4 pto vida máxima
-                Cooldown: 0
-            }
-        }
-    
-    },
-    Armor_3 :
-    {
-        Consumible: false,
-        Level: 3,
-        Price: 80,
-        Effect:
-        {
-            Target: "self",
-            Data:
-            {
-                Attribute: "health",
-                Quantity: 6,        // +6 pto vida máxima
-                Cooldown: 0
-            }
-        }
-    
-    },
-    Bow_1 :
-    {
-        Consumible: false,
-        Level: 1,
-        Price: 15,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-                Attribute: "distance",
-                Quantity: 4, // Alcance: 4 jugadores.
-                Cooldown: 2  // Velocidad de carga: 2 s
-            }
-        },
-        Sprite:
-        {
-            ID : "Bow_1",
-            Scale:0.5
-        }
-    
-    },
-    Bow_2 :
-    {
-        Consumible: false,
-        Level: 2,
-        Price: 30,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-                Attribute: "distance",
-                Quantity: 6,        //Alcance: 6 jugadores
-                Cooldown: 1.5       //Velocidad de carga: 1.5 s
-            }
-        },
-        Sprite:
-        {
-            ID : "Bow_2",
-            Scale:0.5
-        }
-    
-    },
-    Bow_3 :
-    {
-        Consumible: false,
-        Level: 3,
-        Price: 60,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-                Attribute: "distance",
-                Quantity: 8,        //Alcance: 8 jugadores
-                Cooldown: 1         //Velocidad de carga: 1.5 s
-            }
-        },
-        Sprite:
-        {
-            ID : "Bow_3",
-            Scale:0.5
-        }
-    
-    },
-    Shield_1 :
-    {
-        Consumible: true,
-        Level: 1,
-        Price: 20,
-        Effect:
-        {
-            Target: "self",
-            Data:
-            {
-                Attribute: "none",
-                Quantity: 2,        //Bloquea 2 puntos de ataques
-                Cooldown: 0
-            }
-        },
-        Sprite:
-        {
-            ID : ""
-        }
-    
-    },
-    Shield_2 :
-    {
-        Consumible: true,
-        Level: 2,
-        Price: 40,
-        //Units: 1
-        //blocks 4 pto ataque
-        Effect:
-        {
-            Target: "self",
-            Data:
-            {
-                Attribute: "none",
-                Quantity: 4,        //Bloquea 4 puntos de ataques
-                Cooldown: 0
-            }
+            Sprite:"",
+            Icon:"",
         }
     },
-    Shield_3 :
+    Armor2:
     {
-        Consumible: true,
-        Level: 3,
-        Price: 80,
-        //Units: 1
-        //blocks 6 pto ataque
-        Effect:
+        Type: "Armor",
+        ExtraMaxHealth:0,
+        Price:0,
+        Level:"",
+        Images:
         {
-            Target: "self",
-            Data:
-            {
-                Attribute: "none",
-                Quantity: 6,    //Bloquea 6 puntos de ataques
-                Cooldown: 0
-            }
+            Sprite:"",
+            Icon:"",
         }
-    
     },
-    Boots :
+    Armor3:
     {
-        Consumible: false,
-        //Level: 1
-        Price: 50,
-        //Units: 1
-        Effect:
+        Type: "Armor",
+        ExtraMaxHealth:0,
+        Price:0,
+        Level:"",
+        Images:
         {
-            Target: "self",
-            Data:
-            {
-                Attribute: "speed",
-                Quantity: 0.5,      // +50% velocidad movimiento
-                Cooldown: 0
-            }
+            Sprite:"",
+            Icon:"",
         }
-    
     },
-    
-
+    Arrow1:
+    {
+        Type: "Proyectile",
+        Damage:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Arrow2:
+    {
+        Type: "Proyectile",
+        Damage:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Bow1:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Bow2:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Bow3:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Sword0:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Sword1:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Sword2:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Sword3:
+    {
+        Type: "Weapon",
+        Damage:0,
+        Cooldown:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Shield1:
+    {
+        Type: "Shield",
+        MaxHits:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Shield2:
+    {
+        Type: "Shield",
+        MaxHits:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
+    Shield3:
+    {
+        Type: "Shield",
+        MaxHits:0,
+        Price:0,
+        Level:"",
+        Images:
+        {
+            Sprite:"",
+            Icon:"",
+        }
+    },
     Potion :
     {
-        Consumible: true,
-        Price:  15,
-        Effect: 
+        Type: "Potion",
+        Health:  0,
+        Price:0,
+        Level:1,
+        Images:
         {
-        Target:   "self",  
-        Data:  3 
+            Sprite:"",
+            Icon:"",
         }
     
     },
     Radar :
     {
-        Consumible: true,
-        Price: 30,
-        Effect: 
+        Type: "Radar",
+        Area:  0,
+        Price:0,
+        Level:1,
+        Images:
         {
-        Target:   "self",  
-        Data:  -1 
+            Sprite:"",
+            Icon:"",
         }
     },
     Grenade :
     {
-        Consumible: true,
-        Price: 20,
-        Effect: 
+        Type: "Proyectile",
+        Damage:  0,
+        Price:0,
+        Level:1,
+        Images:
         {
-            Target:   "other",  
-            Data:
-            {
-                maxDistance : 1,
-                speed       : 0,
-                damage      : 3,        //+3 pto daño a los enemigos en un área de una casilla
-                time        : 1
-            }
+            Sprite:"",
+            Icon:"",
         }
     
     },
-    Arrow_1:
+    Boots :
     {
-        Consumible: true,
-        Price: 1,
-        Effect:
+        Type: "Boots",
+        Speed:0,
+        Price:0,
+        Level:1,
+        Images:
         {
-            Target: "other",
-            Data:
-            {
-            speed: 1,
-            damage: 1,              // +1 pto daño por flecha
-            time: -1
-            }
-        }
-    },
-    Arrow_2:
-    {
-        Consumible: true,
-        Level: 2,
-        Price: 3,
-        Effect:
-        {
-            Target: "other",
-            Data:
-            {
-            speed: 1,
-            damage: 2,      //2 puntos de daño por flecha
-            time: -1
-            }
+            Sprite:"",
+            Icon:"",
         }
     }
 }
